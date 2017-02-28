324d457964aa07d9918fc0d79af0c586cc77f05c
st031414 <st031414kcg@gmail.com>
2017-02-28 10:51:26 +0900

    day1.js fixed

--- day1/js/day1.js
+++ day1/js/day1.js
@@ -2,11 +2,12 @@ console.log("aaa");
 
 var Cat = function(name){
 	this.name = name;
-	Cat.prototype.run = function(){
-		console.log(this.name+"‚ª‘–‚é");
-	}
 }
 
-var tama = new cat("dede");
+Cat.prototype.run = function(){
+		console.log(this.name+"‚ª‘–‚é");
+	}
+
+var tama = new Cat("dede");
 tama.run();
 console.log(tama.name);
